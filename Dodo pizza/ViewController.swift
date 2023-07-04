2b732181337254d4e767c5161bda4ab8d6b586a8
Seneca242 <flabbergasted242@gmail.com>
2023-07-04 23:07:09 +0300

    just started making first viewController

--- Dodo pizza/ViewController.swift
+++ Dodo pizza/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .orange
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
 
+}
 
+extension ViewController {
+    private func setupViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
 }
 
