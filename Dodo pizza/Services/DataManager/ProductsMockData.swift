dbb122806c962e5f9ed78a79c8f42e54b175bc56
Seneca242 <flabbergasted242@gmail.com>
2023-11-01 19:09:45 +0200

    made getting products via API

---
+++ Dodo pizza/Services/DataManager/ProductsMockData.swift
@@ -0,0 +1,8 @@
+//
+//  ProductsMockData.swift
+//  Dodo pizza
+//
+//  Created by Dmitry Dmitry on 31.10.2023.
+//
+
+import Foundation
---
+++ Dodo pizza/Services/NetworkManager/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Dodo pizza
+//
+//  Created by Dmitry Dmitry on 31.10.2023.
+//
+
+import Foundation
